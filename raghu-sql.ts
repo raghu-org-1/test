aad8524a3d47fb193bb7f36dfecb5718c05adff7
RajeshKM12 <159002418+RajeshKM12@users.noreply.github.com>
2025-02-06 14:32:26 +0530

    Create raghu-sql.ts

---
+++ raghu-sql.ts
@@ -0,0 +1,8 @@
+findAll(): Promise < Company[] > {
+    return this.companiesModel.findAll();
+}
+  async findByName(name: string): Promise < Company[] > {
+    return await this.companiesModel.sequelize.query(
+        `SELECT * FROM company WHERE name = '${name}'`,
+    );
+}
